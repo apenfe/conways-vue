73b49c60601b745c85dd281d028e78a9453328b0
apenfe <adrian1414@hotmail.es>
2024-07-28 19:42:57 +0200

    comunicacion vertical entre componentes

---
+++ src/components/ConwaysForm.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <form>
+
+        <label for="rows">Filas: </label>
+        <input v-model="filas" type="number" name="rows" id="rows" placeholder="Nº filas" maxlength="20" minlength="1">
+
+        <br/>
+
+        <label for="colums">Columnas: </label>
+        <input v-model="columnas" type="number" name="colums" id="colums" placeholder="Nº columnas" maxlength="95" minlength="1">
+
+        <div v-if="columnas > 0 && filas > 0">
+
+            <h5>Has seleccionado {{filas}} filas y {{columnas}} columnas</h5>
+            <br>
+            <input @click="handleClick" type="submit" value="Enviar">
+
+        </div>
+        <div v-else>
+            <h5>Selecciona una cantiddad valida de filas y columnas</h5>
+        </div>
+
+    </form>
+
+</template>
+
+<script lang="js" setup>
+
+import {ref, defineEmits} from 'vue';
+
+let columnas = ref(0);
+let filas = ref(0);
+
+const emit = defineEmits(['createTable']);
+
+const handleClick = ()=>{
+
+    emit("createTable", filas.value, columnas.value)
+    filas.value = ""
+    columnas.value=""
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ConwaysGame.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <h1>Conway's - Game Of Life</h1>
+    <ConwaysForm @createTable="updateTable"></ConwaysForm>
+
+    <div id="zepa">
+
+        <table border="1">
+
+            <tr v-for="(index) in filas" :key="index">
+
+                <td v-for="(index2) in columnas" :key="index2">
+                    x
+                </td>
+                
+            </tr>
+
+        </table>
+
+    </div>
+
+</template>
+
+<script lang="js" setup>
+
+import ConwaysForm from './ConwaysForm.vue';
+import {ref} from 'vue';
+
+let filas = ref(0);
+let columnas = ref(0);
+
+function updateTable(row,col){
+    filas.value = row;
+    columnas.value = col;
+}
+
+</script>
+
+<style scoped>
+
+</style>
